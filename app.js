932760d9ea5775ce138639a81febaac25f07011b
jennyunh <jennyunh@gmail.com>
2022-07-12 17:29:28 -0700

    post form data, added middleware function to parse data

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const express = require('express');
 const app = express();
 
 
+//handles incoming requests. Add a handler that should be executed on all
+//incoming requests (middleware function)
+//url encoded is a method that is an incoming request data parser.
+//If the request has form data, it will translate data into js object
+app.use(express.urlencoded({extended: false}));
+
 //express by default sets the status code to 200
 //get method allows us to define a request handler for incoming get requests.
 //1st parameter: the path
@@ -21,10 +27,19 @@ response.send('<h1>' + new Date().toISOString() + '</h1>')
 
 
 //another route for just / path
+//the name attribute in the input element gives us a key we can use to extract this data
+//from the request object's body property
 app.get('/', function(request, response) {
-  response.send("<h1>Hello World!</h1>")
+  response.send("<form action='/storeUser' method='POST'><label>your name</label><input type='text' name='username'><button>submit</button></form>")
 })
 
+
+//route for POST method after submitting the form
+app.post('/storeUser', function(req, res){
+const username = req.body.username;
+console.log(username);
+res.send('<h1>Username stored!</h1>');
+})
 
 //listen to port 3000
 app.listen(3000)
